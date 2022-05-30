18289ac8053a6f3b2f1273c65574cdbdb878b46d
khaled39ab <khaled39ab@gmail.com>
2022-05-30 10:27:41 +0600

    created a fancy 404 page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NotFound.css'
 
 const NotFound = () => {
     return (
-        
+        <section class="page_404">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 ">
+                        <div class="col-sm-10 col-sm-offset-1  text-center">
+                            <div class="four_zero_four_bg">
+                                <h1 class="text-center ">404</h1>
+
+
+                            </div>
+
+                            <div class="content_box_404">
+                                <h3 class="h2">
+                                    Look like you're lost
+                                </h3>
+                                <p>the page you are looking for is not available!</p>
+
+                                <Link  className="link_404" to={'/'}>Go to Home</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
