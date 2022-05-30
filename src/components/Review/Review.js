888b9b9a0bc29c84b6a8595ce3a325efc769afb9
khaled39ab <khaled39ab@gmail.com>
2022-05-30 12:54:33 +0600

    reviews not working

---
+++ src/components/Review/Review.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Review = (props) => {
+    const {name, review, rating, img} = props.review;
+    console.log(name);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Review;
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import useReviews from '../../hooks/useReviews';
+import Review from '../Review/Review';
 
 const Reviews = () => {
+    const [reviews, setreviews] = useReviews();
     return (
         <div>
+            {
+                // reviews.map(review =><Review
+                //     key={review.id}
+                //     review={review}
+                // ></Review>)
+            }
             <h1>this is reviews</h1>
+            {
+                reviews.map(review => console.log(review))
+            }
         </div>
     );
 };
